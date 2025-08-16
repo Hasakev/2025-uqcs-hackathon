2621ea0d04b5de54b5c633cadf793fba430b875c
Felix Pountney <u7309495@anu.edu.au>
2025-08-16 23:17:07 +1000

    print for json equiv of db variables

--- backend/app/models/db.py
+++ backend/app/models/db.py
@@ -12,6 +12,13 @@ class User(db.Model):
     password = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def to_json(self):
+        """Converts the User object to a JSON-serializable dictionary."""
+        return {
+            "username": self.username,
+            "email": self.email,
+        }
+
 class BetType(enum.Enum):
     Monetary = 1
     Text = 2
@@ -25,7 +32,7 @@ class BetStatus(enum.Enum):
 
 class Bets(db.Model):
     __tablename__ = "bets"
-    uuid = db.Column(db.Uuid(), primary_key=True, default=uuid.uuid4())
+    uuid = db.Column(db.Uuid, primary_key=True, default=uuid.uuid4)
     u1 = db.Column(db.String(80))
     u2 = db.Column(db.String(80))
     type = db.Column(db.Enum(BetType))
@@ -39,3 +46,22 @@ class Bets(db.Model):
     wager1 = db.Column(db.Float())
     wager2 = db.Column(db.Float())
     description = db.Column(db.Text())
+
+    def to_json(self):
+        """Converts the Bets object to a JSON-serializable dictionary."""
+        return {
+            "uuid": str(self.uuid),
+            "u1": self.u1,
+            "u2": self.u2,
+            "type": self.type.name if self.type else None,
+            "status": self.status.name if self.status else None,
+            "coursecode": self.coursecode,
+            "year": self.year,
+            "semester": self.semester,
+            "assessment": self.assessment,
+            "upper": self.upper,
+            "lower": self.lower,
+            "wager1": self.wager1,
+            "wager2": self.wager2,
+            "description": self.description,
+        }
