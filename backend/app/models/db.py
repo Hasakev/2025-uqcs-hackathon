27d3711449d4ac5ca2cd17b7088794322f053b5b
Felix Pountney <u7309495@anu.edu.au>
2025-08-16 20:21:41 +1000

    added uuid's for use i betting database

--- backend/app/models/db.py
+++ backend/app/models/db.py
@@ -2,6 +2,7 @@ import datetime
 import email
 from . import db
 import enum
+import uuid
 
 
 
@@ -24,11 +25,15 @@ class BetStatus(enum.Enum):
 
 class Bets(db.Model):
     __tablename__ = "bets"
-    uuid = db.Column(db.String(80), primary_key=True)
+    uuid = db.Column(db.Uuid(), primary_key=True, default=uuid.uuid4)
     u1 = db.Column(db.String(80))
     u2 = db.Column(db.String(80))
     type = db.Column(db.Enum(BetType))
     status = db.Column(db.Enum(BetStatus))
+    coursecode = db.Column(db.String(80))
+    year = db.Column(db.Integer())
+    semester = db.Column(db.Integer())
+    assessment = db.Column(db.String(80))
     upper = db.Column(db.Integer())
     lower = db.Column(db.Integer())
     wager1 = db.Column(db.Float())
