d88613ea3cd9557dd499dd2e3228713c2c636201
Kevin Luo <hasakev@gmail.com>
2025-08-16 14:58:21 +1000

    Hi Felix pt. 2

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,16 @@
 from app.views.routes import BB_BASE_URL, BB_CLIENT_ID, BB_CLIENT_SECRET, BB_REDIRECT_URI, WEB_ORIGIN
 from flask import Flask
 from app.src.token_manager import TokenManager
+from app.models.db import db
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
     from .views.routes import api, SECRET_KEY
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
     app.register_blueprint(api)
     app.config["SECRET_KEY"] = SECRET_KEY
     app.config["BB_BASE_URL"] = BB_BASE_URL
